a40030dcf0aaa38ae724aba70f9681fe16d342b4
VieraJulian <julianviera2019@hotmail.com>
2025-02-10 17:19:49 -0300

    Eureka Server

---
+++ eureka-server/src/test/java/com/socialred2025/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialred2025.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
