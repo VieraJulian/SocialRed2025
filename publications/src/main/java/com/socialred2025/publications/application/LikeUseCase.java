fffd1f5fb0395eb3e4ee9c0f63099dd1555bf0da
VieraJulian <julianviera2019@hotmail.com>
2025-04-30 18:18:13 -0300

    Like error fixed

--- publications/src/main/java/com/socialred2025/publications/application/LikeUseCase.java
+++ publications/src/main/java/com/socialred2025/publications/application/LikeUseCase.java
@@ -4,11 +4,13 @@ import com.socialred2025.publications.application.dto.LikeDTO;
 import com.socialred2025.publications.application.dto.LikeRequestDTO;
 import com.socialred2025.publications.application.exception.LikeAlreadyExistsException;
 import com.socialred2025.publications.application.exception.LikeNotFoundException;
+import com.socialred2025.publications.application.exception.PublicationNotFoundException;
 import com.socialred2025.publications.application.exception.UnableToDeleteLikeException;
 import com.socialred2025.publications.application.mapper.ILikeMapper;
 import com.socialred2025.publications.domain.Like;
 import com.socialred2025.publications.infrastructure.inputport.ILikeInputPort;
 import com.socialred2025.publications.infrastructure.outputport.ILikeRepository;
+import com.socialred2025.publications.infrastructure.outputport.IPublicationRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,13 +20,21 @@ public class LikeUseCase implements ILikeInputPort {
 
     private final ILikeMapper likeMapper;
 
-    public LikeUseCase(ILikeRepository likeRepository, ILikeMapper likeMapper) {
+    private final IPublicationRepository publicationRepository;
+
+    public LikeUseCase(ILikeRepository likeRepository, ILikeMapper likeMapper, IPublicationRepository publicationRepository) {
         this.likeRepository = likeRepository;
         this.likeMapper = likeMapper;
+        this.publicationRepository = publicationRepository;
     }
 
     @Override
-    public LikeDTO addLike(Long userId, LikeRequestDTO likeRequestDTO) throws LikeAlreadyExistsException {
+    public LikeDTO addLike(Long userId, LikeRequestDTO likeRequestDTO) throws LikeAlreadyExistsException, PublicationNotFoundException {
+        long publicationId = likeRequestDTO.getPublicationId();
+
+        if (!publicationRepository.existsPublicationById(publicationId)) {
+            throw new PublicationNotFoundException("Publication not found with id: " + publicationId);
+        }
 
         if (likeRepository.findByUserIdAndPublicationId(userId, likeRequestDTO.getPublicationId()).isPresent()) {
             throw new LikeAlreadyExistsException("The user has already liked this post.");
--- publications/src/main/java/com/socialred2025/publications/infrastructure/inputport/ILikeInputPort.java
+++ publications/src/main/java/com/socialred2025/publications/infrastructure/inputport/ILikeInputPort.java
@@ -4,10 +4,11 @@ import com.socialred2025.publications.application.dto.LikeDTO;
 import com.socialred2025.publications.application.dto.LikeRequestDTO;
 import com.socialred2025.publications.application.exception.LikeAlreadyExistsException;
 import com.socialred2025.publications.application.exception.LikeNotFoundException;
+import com.socialred2025.publications.application.exception.PublicationNotFoundException;
 import com.socialred2025.publications.application.exception.UnableToDeleteLikeException;
 
 public interface ILikeInputPort {
 
-    LikeDTO addLike(Long userId, LikeRequestDTO likeRequestDTO) throws LikeAlreadyExistsException;
+    LikeDTO addLike(Long userId, LikeRequestDTO likeRequestDTO) throws LikeAlreadyExistsException, PublicationNotFoundException;
     String removeLike(Long userId, LikeRequestDTO likeRequestDTO) throws LikeNotFoundException, UnableToDeleteLikeException;
 }
