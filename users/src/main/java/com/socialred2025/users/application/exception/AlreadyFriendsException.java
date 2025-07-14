daa84fedd6ff18128790af7585cefb54e67de729
VieraJulian <julianviera2019@hotmail.com>
2025-07-14 19:46:28 -0300

    Add custom exceptions

---
+++ users/src/main/java/com/socialred2025/users/application/exception/AlreadyFriendsException.java
@@ -0,0 +1,7 @@
+package com.socialred2025.users.application.exception;
+
+public class AlreadyFriendsException extends Exception {
+    public AlreadyFriendsException(String message) {
+        super(message);
+    }
+}
---
+++ users/src/main/java/com/socialred2025/users/application/exception/SelfFriendException.java
@@ -0,0 +1,7 @@
+package com.socialred2025.users.application.exception;
+
+public class SelfFriendException extends Exception {
+    public SelfFriendException(String message) {
+        super(message);
+    }
+}
